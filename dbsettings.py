3b67191b2d6465694e46c43bc8e23919f39c405c
Kwanjaid <145360527+Kwanjaid@users.noreply.github.com>
2023-12-04 20:07:27 +0700

    Kus01 New-version Update

--- dbsettings.py
+++ dbsettings.py
@@ -1,12 +1,21 @@
-domain = "kitcherfromlocal.vercel.app"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+domain = os.getenv('API_DOMAIN')
 
 # host = "sql12.freesqldatabase.com"
 # user = "sql12661888"
 # password = "qGTLcNPmDQ"
 # db = "sql12661888"
 
-db = "kitcher"
-user = "m1jbcw564xabe9b9e1ax"
-host = "aws.connect.psdb.cloud"
-password = "pscale_pw_C6Tn0cZ3SHIQUvvuGEfbNTHvMAiYmscTLDuB8ExGNT3"
+# db = os.getenv("kitcher")
+# user = "m1jbcw564xabe9b9e1ax"
+# host = "aws.connect.psdb.cloud"
+# password = "pscale_pw_C6Tn0cZ3SHIQUvvuGEfbNTHvMAiYmscTLDuB8ExGNT3"
 
+db = os.getenv('PLANETSCALE_API_DB')
+user = os.getenv('PLANETSCALE_API_USER')
+host = os.getenv('PLANETSCALE_API_HOST')
+password = os.getenv('PLANETSCALE_API_PASSWORD')
